19ca741c58a76506f37e27594d7a230eb1a50693
David Dawson <me@daviddawson.me>
2022-09-02 15:54:11 +0100

    some doc updates

--- src/events/adapter/AggregateObservationAdapter.ts
+++ src/events/adapter/AggregateObservationAdapter.ts
@@ -7,13 +7,48 @@ class InternalEv extends EventEmitter {}
 
 let emitter = new InternalEv();
 
+
+class AggregateObservationAdapter implements EventAdapter {
+  readonly name = "aggregate-observer";
+
+  constructor(readonly streamsToSubscribe: string[]) {
+    logger.debug("Aggregate Observer Started for streams ", streamsToSubscribe);
+  }
+
+  readonly consumerGroup = "aggregate-observation-" + uuid.v4();
+
+  async handleEvent(event: EventicleEvent): Promise<void> {
+    logger.trace("AR Observer has an event", event);
+    emitter.emit("event", event);
+  }
+}
+
+let VIEW: AggregateObservationAdapter;
+
 /**
+ * Create a new EventAdapter with a dynamic Consumer group id that will subscribe to the topics for the
+ * given AggregateRoots
+ *
+ * @param aggregates the list of aggregate roots to observe. Their topic names will be extracted.
+ */
+export function createAggregateObservationAdapter<AR extends AggregateRoot>(
+  aggregates: { new (): AR }[]
+) {
+  VIEW = new AggregateObservationAdapter(
+    aggregates.map((value) => new value().type)
+  );
+  return VIEW;
+}
+export const aggregateObservationAdapter = () => VIEW;
+
+/**
+ * Block for an asynchronous, event based, workflow to occur.
+ *
  * Reads streams and allows short lived observers to be notified if a particular AggregateRoot instance has been modified.
  * This is picked out based on the `domainId` of the event.
  *
  * This allows a synchronous API to block and wait for asynchronous changes to finish occurring before continuing processing
  *
- * @example
  * For example, given a `User` {@link AggregateRoot}, you can wait for a Saga (or other operational component)
  * to approve a user account, by blocking until the given `User` has raised the `user.approved` event.
  *
@@ -41,7 +76,7 @@ let emitter = new InternalEv();
  *     })
  *   }
  * }
- * ``
+ * ```
  * First register the observer. This will open a new event subscription, with a dynamically generated consumer group.
  * All events on the topic will be received by this instances, so be mindful of doing this on topics with large volumes
  * of data.
@@ -77,57 +112,6 @@ let emitter = new InternalEv();
  *
  * }
  * ```
- */
-class AggregateObservationAdapter implements EventAdapter {
-  readonly name = "aggregate-observer";
-
-  constructor(readonly streamsToSubscribe: string[]) {
-    logger.debug("Aggregate Observer Started for streams ", streamsToSubscribe);
-  }
-
-  readonly consumerGroup = "aggregate-observation-" + uuid.v4();
-
-  async handleEvent(event: EventicleEvent): Promise<void> {
-    logger.trace("AR Observer has an event", event);
-    emitter.emit("event", event);
-  }
-}
-
-let VIEW: AggregateObservationAdapter;
-
-/**
- * Create a new EventAdapter with a dynamic Consumer group id that will subscribe to the topics for the
- * given AggregateRoots
- *
- * @param aggregates the list of aggregate roots to observe. Their topic names will be extracted.
- */
-export function createAggregateObservationAdapter<AR extends AggregateRoot>(
-  aggregates: { new (): AR }[]
-) {
-  VIEW = new AggregateObservationAdapter(
-    aggregates.map((value) => new value().type)
-  );
-  return VIEW;
-}
-export const aggregateObservationAdapter = () => VIEW;
-
-/**
- * Observe an aggregate root instance.
- *
- * The handler will be executed :-
- * * once with the current state of the aggregate root instance, the event property will be empty
- * * once for every subsequent event that modifies the aggregate root instance
- *
- * The ar property will contain the state of the AR _after_ the event has been applied to it. The ar has been persisted before the handler observes it.
- *
- * This observer operates on a shared eventclient consumer on the underlying aggregate stream, and so does not block
- * event replay per observer.  The exec is not async for this reason and should not perform any expensive processing or it will
- * cause a slowdown in throughput across all other observers.
- *
- * It is possible that events may be presented in parallel to the exec function. If you expect that a single AR instance mey be in a data race,
- * do not use this shared observer view, and build a dedicated event adapter instead.
- *
- * @see AggregateObservationAdapter
  */
 export async function aggregateObserver<AR extends AggregateRoot>(
   aggregateType: { new (...params): AR },
